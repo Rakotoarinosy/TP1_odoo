8c74904731b67c04eebe1c566e4b44c296dcdd9c
Rakotoarinosy <fehizoroarinosy@gmail.com>
2024-11-28 15:28:42 +0300

    architecture of sale extension

---
+++ custom_addons/vente_extension/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Vente Extension',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Extension pour gérer les variations de prix, les coupons et les tendances',
+    'depends': ['base', 'sale', 'product'],
+    'data': [
+        
+    ],
+    'installable': True,
+    'application': True,
+}
